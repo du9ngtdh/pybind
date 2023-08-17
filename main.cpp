e7b850295f7cf06eda64af3175613aafd355192d
du9ngtdh <du9ngtdh@gmail.com>
2023-08-17 13:00:48 +0700

    update project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <vector>
+#include <iostream>
+namespace py = pybind11;
+
+class Test
+{
+public:
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+PYBIND11_MODULE(my_module, m)
+{
+    py::class_<Test>(m, "Test")
+        .def(py::init<>())
+        .def("add", &Test::add);
+}
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+
+# Tên module Python sẽ được tạo ra
+module_name = "my_module"
+
+# Tên tệp mã nguồn C++
+source_file = "main.cpp"
+
+# Tạo extension
+ext_modules = [
+    Pybind11Extension(
+        module_name,
+        [source_file],
+        cxx_std=14,
+    ),
+]
+
+# Setup thông tin cho setuptools
+setup(
+    name=module_name,
+    version="0.1",
+    author="Your Name",
+    description="A Python C++ extension example",
+    ext_modules=ext_modules,
+)
+# python setup.py build_ext --inplace
